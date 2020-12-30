f187cbe875d6c25145f54eeae9f035cd2c1072bb
TristenMaetzold <72782148+TristenMaetzold@users.noreply.github.com>
2020-12-29 19:17:53 -0600

    Add files via upload

--- Access Demo/code.js
+++ Access Demo/code.js
@@ -1,3 +1,32 @@
-function changeSlides(slides) {
-  console.log("Did I work?");
+var slideNumber = 0;
+var slide = document.getElementsByClassName('slide');
+
+function changeSlides(n) {
+  for (var i = 0; i < slide.length; i++) {
+    if (i == n) {
+      slide[n].classList.add("show")
+      slide[n].classList.remove("hide")
+      slideNumber = n;
+  } else {
+      slide[i].classList.add("hide")
+      slide[i].classList.remove("show")
+    }
+  }
+  console.log(slideNumber);
+}
+
+function nextSlide() {
+  slideNumber++;
+    if (slideNumber > slide.length){
+      slideNumber = 0
+    }
+    changeSlides(slideNumber);
+}
+
+function prevSlide() {
+  slideNumber--;
+    if (slideNumber < 0) {
+      slideNumber = slide.lenght-1
+    }
+    changeSlides(slideNumber);
 }
